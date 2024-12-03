09706944f063820cfdd87db9a9bbf42b30eea375
dele-dev <awodeleakinwunmi@gmail.com>
2024-12-03 02:39:43 -0800

    adding 3_d code; orbit control function

---
+++ 3d_model/src/main.js
@@ -0,0 +1,66 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+
+/***
+ * 
+ * set up a scene 
+ *  
+ *  */ 
+const scene = new THREE.Scene();
+
+
+// create geometyry and mesh
+
+const circleGeometry = new THREE.CircleGeometry;
+const circleMaterial = new THREE.MeshBasicMaterial(
+  {
+    color:"red",
+  }
+);
+
+// create Mesh 
+const roundMesh = new THREE.Mesh(
+  circleGeometry,
+  circleMaterial
+)
+
+// add mesh to scene
+scene.add(roundMesh)
+
+
+// get width and height of the window
+const width_ = window.innerWidth;
+const height_ = window.innerHeight;
+
+
+// initialize the camera
+const camera = new THREE.PerspectiveCamera(35,
+  width_/height_,
+  0.1,
+  30
+);
+
+camera.position.z = 5 ;
+
+// add camera to scene
+//scene.add(camera);
+
+// create render
+const canvas = document.querySelector("canvas.threejs");
+
+const renderer =   new THREE.WebGLRenderer({
+  canvas : canvas 
+});
+
+
+//instantiate the controls
+const controls = new OrbitControls(camera,canvas);
+
+renderer.setSize(width_,height_);
+
+// render the scene together with the camera
+renderer.render(scene,camera);
+
+
+console.log(scene,roundMesh)
