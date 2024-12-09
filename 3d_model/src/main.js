f7fa756b54c61681670fcd79f614c6c3a88636b6
MTS CLASS <mtsclass@MTSCLASS0005.local>
2024-12-09 09:14:20 -0500

    add light to scene

--- 3d_model/src/main.js
+++ 3d_model/src/main.js
@@ -16,8 +16,8 @@ const cubeGeometry = new THREE.BoxGeometry(1,1,1);
 // const cubeGeometry = new THREE.BoxGeometry(1,1,1,2,2,2);
 // const cubeGeometry = new THREE.SphereGeometry(1,1 6,16);
 //  const cubeGeometry = new THREE.TorusKnotGeometry();
- 
-
+const planeGeometry = new THREE.PlaneGeometry(1,1);
+const torusKnotGeometry = new THREE.TorusKnotGeometry(0.5,0.15, 100, 16);
 
 // create custome geometry
 const vertices = new Float32Array([
@@ -32,13 +32,15 @@ const geometry = new THREE.BufferGeometry();
 geometry.setAttribute('position', bufferAttribute);
 
 
-const cubeMaterial = new THREE.MeshBasicMaterial(
-  {
-    color: 0x00ff00,
-    transparent: true,
-    opacity:0.5
-  }
-);
+// const cubeMaterial = new THREE.MeshBasicMaterial(
+//   {
+//     color: 0xffffff,
+//     transparent: true,
+//     opacity:0.5
+//   }
+// );
+
+const cubeMaterial = new THREE.MeshLambertMaterial();
 
 // const circleMaterial = new THREE.MeshBasicMaterial(
 //   {
@@ -56,17 +58,17 @@ const cubeMesh = new THREE.Mesh(
 // cubeMesh.rotation.x = THREE.MathUtils.degToRad(90);
 
 const cubeMesh1 = new THREE.Mesh(
-  cubeGeometry,
+  torusKnotGeometry,
   cubeMaterial
 );
 
 cubeMesh1.position.x = 1.5;
 
-// const cubeMesh2 = new THREE.Mesh(
-//   cubeGeometry,
-//   circleMaterial
-// );
-// cubeMesh2.position.x = -2;
+const plane = new THREE.Mesh(
+  planeGeometry,
+  cubeMaterial
+);
+plane.position.x = -1.5;
 
 // add mesh to scene
 // const group = new THREE.Group();
@@ -78,14 +80,22 @@ cubeMesh1.position.x = 1.5;
 cubeMaterial.side = THREE.FrontSide;
 cubeMaterial.fog =false;
 
-const fog = new THREE.Fog(0xffffff,1,10);
+// const fog = new THREE.Fog(0xffffff,1,10);
+// scene.background = new THREE.Color("black");
+// scene.fog =fog;
 
+scene.add(cubeMesh);
+scene.add(cubeMesh1);
+scene.add(plane);
 
-scene.background = new THREE.Color("white");
-scene.fog =fog;
-scene.add(cubeMesh)
-scene.add(cubeMesh1)
+// initialize the light
+const light = new THREE.AmbientLight(0xffffff, 0.2) ;
+scene.add(light);
 
+//  create point light
+const pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.position.set(5,5 ,5);
+scene.add(pointLight);
 
 // get width and height of the window
 const width_ = window.innerWidth;
